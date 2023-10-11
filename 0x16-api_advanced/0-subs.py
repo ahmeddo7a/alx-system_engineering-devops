9d64b62c623b9940689e2ac546e138c1b757929e
ahmeddo7a <ahmedholdo@gmail.com>
2023-10-11 13:47:01 -0700

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,9 +12,9 @@ def number_of_subscribers(subreddit):
     header = {'User-Agent': 'CustomClient/1.0'}
     req = requests.get(url, headers=header, allow_redirects=False)
     if req.status_code != 200:
-        return(0)
+        return (0)
     req = req.json()
     if "data" in req:
         return (req.get("data").get("subscribers"))
     else:
-        return(0)
+        return (0)
