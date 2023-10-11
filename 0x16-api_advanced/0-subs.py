b678574a1bad761e1bfe0f10fcd487bfc7141a47
ahmeddo7a <ahmedholdo@gmail.com>
2023-10-11 13:38:56 -0700

    first task

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+function that queries reddit api and returns number
+of subs
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+        """
+            function definition of getting number of subs
+                """
+                    url = f"https://api.reddit.com/r/{subreddit}/about"
+                        header = {'User-Agent': 'CustomClient/1.0'}
+                            req = requests.get(url, headers=header, allow_redirects=False)
+                                if req.status_code != 200:
+                                        return (0)
+                                            req = req.json()
+                                                if "data" in req:
+                                                        return (req.get("data").get("subscribers"))
+                                                            else:
+                                                                    return (0)
