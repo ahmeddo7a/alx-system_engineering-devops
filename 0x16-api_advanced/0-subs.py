59d0f84f97428e3942401b2edb742791c508e01d
ahmeddo7a <ahmedholdo@gmail.com>
2023-10-11 13:45:17 -0700

    first Task

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
 """
-function that queries reddit api and returns number
-of subs
+Function that query reddit api and return a number 
 """
 import requests
 
-
 def number_of_subscribers(subreddit):
-        """
-            function definition of getting number of subs
-                """
-                    url = f"https://api.reddit.com/r/{subreddit}/about"
-                        header = {'User-Agent': 'CustomClient/1.0'}
-                            req = requests.get(url, headers=header, allow_redirects=False)
-                                if req.status_code != 200:
-                                        return (0)
-                                            req = req.json()
-                                                if "data" in req:
-                                                        return (req.get("data").get("subscribers"))
-                                                            else:
-                                                                    return (0)
+    """
+    function definition to gert number of subs
+    """
+    url = f"https://api.reddit.com/r/{subreddit}/about"
+    header = {'User-Agent': 'CustomClient/1.0'}
+    req = requests.get(url, headers=header, allow_redirects=False)
+    if req.status_code != 200:
+        return(0)
+    req = req.json()
+    if "data" in req:
+        return (req.get("data").get("subscribers"))
+    else:
+        return(0)
