19cc21759db2356a504d448bdd4e407cff01eae8
ahmeddo7a <ahmedholdo@gmail.com>
2023-10-19 06:24:33 -0700

    fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,5 @@
-exec { 'increase-hard-file-limit-for-holberton-user':
-  command => "sed -i '/^holberton hard/s/5/50000/' /etc/security/limits.conf",
-  path    => '/usr/local/bin/:/bin/'
-}
-
-exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/^holberton soft/s/4/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+# change file limit
+exec {'up limit':
+command  => "sed -i 's/4/10000/g; s/5/10000/g' /etc/security/limits.conf",
+provider => shell
 }
